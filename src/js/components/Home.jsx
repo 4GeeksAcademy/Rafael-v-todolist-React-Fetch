dd3b1c04b95fec0f2a7e874c3217679957610284
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-03-06 00:17:37 +0000

    mostrar y crear usuario

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [lista, setLista] = useState([])
+	const [tarea, setTarea] = useState("")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const mostrarTarea = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/rafa")
+			console.log(response)
+			if (response.status == 404) {
+				crearUsuario()
+				return
+			}
+			const data = await response.json()
+			console.log(data.todos)
+			setLista(data.todos)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	const crearUsuario = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/rafa",{
+				method:"POST",
+				headers:{"Content-Type":"application/json"}
+			})
+			console.log (response)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+
+	const agregarTarea = (e) => {
+		e.preventDefault()
+		// console.log(tarea)
+		setLista([...lista, tarea])
+		setTarea("")
+	}
+
+	const borrarTarea = (index) => {
+		let newList = lista.filter((item, i) => i !== index);
+		setLista(newList)
+	}
+
+	useEffect(() => {
+		mostrarTarea()
+	}, [])
+
+	return (
+		<div className="text-center container">
+			<h2>to do list</h2>
+			{/* grega tareas a nuestra lista */}
+			<input className="form-control"
+				value={tarea}
+				onChange={(e) => setTarea(e.target.value)}
+				onKeyDownCapture={(e) => e.key === "Enter" ? agregarTarea(e) : null}
+			/>
+
+			{/* muestra la lista de tareas */}
+			<ul className="list-group mt-3">
+				{lista.map((tarea, index) => ( //map recorre mi array uno x uno que recorra la lista
+					<li className="list-group-item" key={index}>          {/* // colocamos key por el map  */}
+						{tarea.label}
+						{/* borrar tareas */}
+						<button className="btn btn-outline-danger float-end icono" onClick={() => borrarTarea(index)}>X</button>
+					</li>
+
+				))}
+
+
+			</ul>
+
+			<p>tareas pendientes: {lista.length}</p>
+
 		</div>
 	);
 };
